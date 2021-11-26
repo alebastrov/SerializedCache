54fa4520ec658fe9db10f4b23bbdfa92037ea932
inikonov <igor.nikonoff@gmail.com>
2021-11-26 10:58:01 +0200

    prepared for internal object compacting

--- src/Holder.java
+++ src/Holder.java
@@ -1,7 +1,9 @@
 import com.nikondsl.cache.MayBeCompacted;
 
 import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 @MayBeCompacted
 public class Holder {
@@ -9,6 +11,8 @@ public class Holder {
     @MayBeCompacted(ifMoreThen = 120)
     private String veryBigText = "default value";
     private String usual = "default value";
+    @MayBeCompacted
+    private List<InnerHolder> inners = new ArrayList<>();
     @MayBeCompacted
     private byte[] someBytes = "new byte[] values".getBytes(StandardCharsets.UTF_8);
 
@@ -44,13 +48,53 @@ public class Holder {
         this.someBytes = someBytes;
     }
 
+    public List<InnerHolder> getInners() {
+        return inners;
+    }
+
     @Override
     public String toString() {
         return "Holder{" +
                 "someNumber=" + someNumber +
                 ", veryBigText='" + veryBigText + '\'' +
                 ", usual='" + usual + '\'' +
+                ", inners=" + inners +
                 ", someBytes=" + Arrays.toString(someBytes) +
                 '}';
+    }
+
+    public void setInners(List<InnerHolder> inners) {
+        this.inners = inners;
+    }
+
+    @MayBeCompacted
+    public static class InnerHolder {
+        private Integer id;
+        @MayBeCompacted
+        private String expression;
+
+        public Integer getId() {
+            return id;
+        }
+
+        public void setId(Integer id) {
+            this.id = id;
+        }
+
+        public String getExpression() {
+            return expression;
+        }
+
+        public void setExpression(String expression) {
+            this.expression = expression;
+        }
+
+        @Override
+        public String toString() {
+            return "InnerHolder{" +
+                    "id=" + id +
+                    ", expression='" + expression + '\'' +
+                    '}';
+        }
     }
 }
--- src/com/nikondsl/cache/ConcurrentCacheImpl.java
+++ src/com/nikondsl/cache/ConcurrentCacheImpl.java
@@ -23,135 +23,19 @@ public class ConcurrentCacheImpl<K, V> implements Cache<K, V> {
     @Override
     public boolean put(K key, V value) throws CompactingException {
         if (value.getClass().getAnnotation(MayBeCompacted.class) != null) {
-            // compact:
-            // - get all fields supporting compacting
-            // - for each field create a shallow byte[] with compacted form
-            // - replace field value with null
-            try {
-                //collect parameters for POJO class
-                Map<String, Class<?>> parameters = new HashMap<>();
-                Properties flagsForObject = new Properties();
-
-                //put fields to class
-                for (Field field : value.getClass().getDeclaredFields()) {
-                    if (field.getAnnotation(MayBeCompacted.class) == null) {
-                        parameters.put(field.getName(), field.getType());
-                        continue;
-                    }
-                    if (field.getType() != String.class && field.getType() != byte[].class) {
-                        throw new CompactingException("Only strings and byte[] are supported");
-                    }
-                    field.setAccessible(true);
-                    //add new synthetic field
-                    parameters.put(field.getName() + "CompressedCopy", byte[].class);
-                }
-                parameters.put(FLAGS, Properties.class);
-                //create a new class
-                Class clazz = cachedClasses.get(value.getClass().getCanonicalName() + DEEP_COPY);
-                if (clazz == null) {
-                    clazz = createBeanClass(value.getClass().getCanonicalName() + DEEP_COPY, parameters);
-                    cachedClasses.putIfAbsent(value.getClass().getCanonicalName() + DEEP_COPY, clazz);
-                }
-                Object obj = clazz.newInstance();
-                //init a class with values
-                for (Field field : value.getClass().getDeclaredFields()) {
-                    field.setAccessible(true);
-                    MayBeCompacted annotation = field.getAnnotation(MayBeCompacted.class);
-                    if (annotation != null) {
-                        //set synthetic field
-                        //compact old field value
-                        byte[] compressedBytes;
-                        byte[] uncompressedBytes;
-                        if (field.getType() == String.class) {
-                            String uncompressedValue = (String) field.get(value);
-                            uncompressedBytes = uncompressedValue.getBytes(StandardCharsets.UTF_8);
-                        } else {
-                            uncompressedBytes = (byte[]) field.get(value);
-                        }
-                        if (uncompressedBytes.length > annotation.ifMoreThen()) {
-                            compressedBytes = ZipUtil.zip(uncompressedBytes);
-                            flagsForObject.put(field.getName(), "compressed");
-                        } else {
-                            compressedBytes = uncompressedBytes;
-                            flagsForObject.put(field.getName(), "as it is");
-                        }
-                        invokeMethod(SET,
-                                field.getName() + "CompressedCopy",
-                                obj,
-                                new Class[] { byte[].class },
-                                new Object[]{ compressedBytes });
-                        continue;
-                    }
-                    //copy original value
-                    invokeMethod(SET,
-                            field.getName(),
-                            obj,
-                            new Class[] { field.getType() },
-                            new Object[]{ field.get(value) });
-                }
-                //set flags
-                invokeMethod(SET, FLAGS, obj, new Class[] { Properties.class }, new Object[] { flagsForObject });
-
-                //put into cache compressed
-                return map.putIfAbsent(key, obj) != null;
-            } catch (Exception ex) {
-                throw new CompactingException(ex);
-            }
+            Object obj = getCompactedCopy(value);
+            //put into cache compacted object
+            return map.putIfAbsent(key, obj) != null;
         }
-        //put into cache usual
+        //put into cache original object
         return map.putIfAbsent(key, value) != null;
     }
 
     @Override
     public V get(K key) throws CompactingException {
-
         Object v = map.get(key);
         if (v != null && v.getClass().getName().endsWith(DEEP_COPY)) {
-            // de-compass:
-            try {
-                // take the properties
-                Properties flagsForObject = (Properties) invokeGet(FLAGS, v);
-                // create original class
-                String className = v.getClass().getName().replace(DEEP_COPY, "");
-                Class clazz = cachedClasses.get(className);
-                if (clazz == null) {
-                    clazz = Class.forName(className);
-                    cachedClasses.putIfAbsent(className, clazz);
-                }
-                Object toBeSetUp = clazz.newInstance();
-                // copy all fields
-                for (Field field : clazz.getDeclaredFields()) {
-                    field.setAccessible(true);
-                    if (field.getAnnotation(MayBeCompacted.class) != null) {
-                        byte[] compressedBytes = (byte[]) invokeGet(field.getName() + "CompressedCopy", v);
-                        byte[] uncompressedBytes;
-                        if ("compressed".equals(flagsForObject.get(field.getName()))) {
-                            uncompressedBytes = ZipUtil.unZip(compressedBytes);
-                        } else {
-                            uncompressedBytes = compressedBytes;
-                        }
-                        // set original value up
-                        if (field.getType() == String.class) {
-                            String value = new String(uncompressedBytes, StandardCharsets.UTF_8);
-                            field.set(toBeSetUp, value);
-                        } else {
-                            field.set(toBeSetUp, uncompressedBytes);
-                        }
-                        continue;
-                    }
-                    Object value = invokeGet(field.getName(), v);
-                    //copy usual field
-                    field.set(toBeSetUp, value);
-                }
-                return (V) toBeSetUp;
-            } catch (Exception ex) {
-                throw new CompactingException(ex);
-            }
-            // - create a new object to return using default constructor
-            // - get all fields supporting compacting
-            // - for each field look for shallow byte[] with compacted form
-            // - replace field value with real value
-
+            return getOriginalCopy(v);
         }
         return (V) v;
     }
@@ -159,6 +43,127 @@ public class ConcurrentCacheImpl<K, V> implements Cache<K, V> {
     @Override
     public void remove(K key) {
         map.remove(key);
+    }
+
+
+    private Object getCompactedCopy(Object value) throws CompactingException {
+        // compact:
+        // - get all fields supporting compacting
+        // - for each field create a shallow byte[] with compacted form
+        // - replace field value with null
+        try {
+            //collect parameters for POJO class
+            Map<String, Class<?>> parameters = new HashMap<>();
+            Properties flagsForObject = new Properties();
+
+            //put fields to class
+            for (Field field : value.getClass().getDeclaredFields()) {
+                if (field.getAnnotation(MayBeCompacted.class) == null) {
+                    parameters.put(field.getName(), field.getType());
+                    continue;
+                }
+                if (field.getType() != String.class && field.getType() != byte[].class) {
+                    throw new CompactingException("Only strings and byte[] are supported");
+                }
+                field.setAccessible(true);
+                //add new synthetic field
+                parameters.put(field.getName() + "CompressedCopy", byte[].class);
+            }
+            parameters.put(FLAGS, Properties.class);
+            //create a new class
+            Class clazz = cachedClasses.get(value.getClass().getCanonicalName() + DEEP_COPY);
+            if (clazz == null) {
+                clazz = createBeanClass(value.getClass().getCanonicalName() + DEEP_COPY, parameters);
+                cachedClasses.putIfAbsent(value.getClass().getCanonicalName() + DEEP_COPY, clazz);
+            }
+            Object obj = clazz.newInstance();
+            //init a class with values
+            for (Field field : value.getClass().getDeclaredFields()) {
+                field.setAccessible(true);
+                MayBeCompacted annotation = field.getAnnotation(MayBeCompacted.class);
+                if (annotation != null) {
+                    //set synthetic field
+                    //compact old field value
+                    byte[] compressedBytes;
+                    byte[] uncompressedBytes;
+                    if (field.getType() == String.class) {
+                        String uncompressedValue = (String) field.get(value);
+                        uncompressedBytes = uncompressedValue.getBytes(StandardCharsets.UTF_8);
+                    } else {
+                        uncompressedBytes = (byte[]) field.get(value);
+                    }
+                    if (uncompressedBytes.length > annotation.ifMoreThen()) {
+                        compressedBytes = ZipUtil.zip(uncompressedBytes);
+                        flagsForObject.put(field.getName(), "compressed");
+                    } else {
+                        compressedBytes = uncompressedBytes;
+                        flagsForObject.put(field.getName(), "as it is");
+                    }
+                    invokeMethod(SET,
+                            field.getName() + "CompressedCopy",
+                            obj,
+                            new Class[] { byte[].class },
+                            new Object[]{ compressedBytes });
+                    continue;
+                }
+                //copy original value
+                invokeMethod(SET,
+                        field.getName(),
+                        obj,
+                        new Class[] { field.getType() },
+                        new Object[]{ field.get(value) });
+            }
+            //set flags
+            invokeMethod(SET, FLAGS, obj, new Class[] { Properties.class }, new Object[] { flagsForObject });
+
+            //put into cache compressed
+            return obj;
+        } catch (Exception ex) {
+            throw new CompactingException(ex);
+        }
+    }
+
+    private V getOriginalCopy(Object v) throws CompactingException {
+        // de-compass:
+        try {
+            // take the properties
+            Properties flagsForObject = (Properties) invokeGet(FLAGS, v);
+            // create original class
+            String className = v.getClass().getName().replace(DEEP_COPY, "");
+            Class clazz = cachedClasses.get(className);
+            if (clazz == null) {
+                clazz = Class.forName(className);
+                cachedClasses.putIfAbsent(className, clazz);
+            }
+            Object toBeSetUp = clazz.newInstance();
+            // copy all fields
+            for (Field field : clazz.getDeclaredFields()) {
+                field.setAccessible(true);
+                if (field.getAnnotation(MayBeCompacted.class) != null) {
+                    byte[] compressedBytes = (byte[]) invokeGet(field.getName() + "CompressedCopy", v);
+                    byte[] uncompressedBytes;
+                    if ("compressed".equals(flagsForObject.get(field.getName()))) {
+                        uncompressedBytes = ZipUtil.unZip(compressedBytes);
+                    } else {
+                        uncompressedBytes = compressedBytes;
+                    }
+                    // set original value up
+                    if (field.getType() == String.class) {
+                        String value = new String(uncompressedBytes, StandardCharsets.UTF_8);
+                        field.set(toBeSetUp, value);
+                    } else {
+                        field.set(toBeSetUp, uncompressedBytes);
+                    }
+                    continue;
+                }
+                Object value = invokeGet(field.getName(), v);
+                //copy usual field
+                field.set(toBeSetUp, value);
+            }
+            return (V) toBeSetUp;
+        } catch (Exception ex) {
+            throw new CompactingException(ex);
+        }
     }
 
     public enum SetGet {
